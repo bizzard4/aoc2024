73750d20a6a4b43c0bc7c9e1287d60eb061c2f0c
Francois Gingras <francois.gingras89@gmail.com>
2024-12-06 19:14:53 -0500

    Day 2 and Day 3

---
+++ src/main/java/D2.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class D2 {
+    public static void main(String[] args) {
+        InputStream is = d1.class.getClassLoader().getResourceAsStream("d2.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        Stream<String> lines = reader.lines();
+
+        List<Report> reports = new ArrayList<>();
+        for (String l : lines.toList()) {
+            List<Integer> splitted = Arrays.stream(l.split("\\s+")).map(Integer::valueOf).toList();
+            reports.add(new Report(splitted));
+        }
+
+        var safeCnt = 0;
+        for (Report r: reports) {
+            if (r.isSafe()) {
+                safeCnt++;
+            } else {
+                // We try to remove all the level one-by-one and see if one of the alternative report is safe
+                var anySafe = false;
+                for (int i = 0; i < r.levels.size(); i++) {
+                    var arrayCopy = new ArrayList<>(r.levels);
+                    arrayCopy.remove(i);
+                    Report alternativeReport = new Report(arrayCopy);
+                    if (alternativeReport.isSafe()) anySafe = true;
+                }
+                if (anySafe) safeCnt++;
+            }
+        }
+        System.out.println(safeCnt);
+    }
+
+    record Report(List<Integer> levels) {
+        // Number of level per report can vary with a minimum of 5
+        public boolean isSafe() {
+            // Shortcut for strictly increasing/decreasing
+            // Sort nlogn and compare with original list
+            var ascendingCmp = levels.stream().sorted().toList();
+            var descendingCmp = ascendingCmp.reversed();
+
+            var stricklyUp = levels.equals(ascendingCmp);
+            var stricklyDown = levels.equals(descendingCmp);
+
+            var maxGap = 0;
+            var minGap = Integer.MAX_VALUE;
+            for (int i = 0; i < levels.size()-1; i++) {
+                var iNext = i+1;
+                var gap = Math.abs(levels.get(i) - levels.get(iNext));
+                if (gap > maxGap) { maxGap = gap; }
+                if (gap < minGap) { minGap = gap; }
+            }
+            var isSafe = (stricklyUp || stricklyDown) && (minGap > 0) && (maxGap < 4);
+            return isSafe;
+        }
+    }
+}
---
+++ src/main/java/D3.java
@@ -0,0 +1,35 @@
+import jdk.jshell.spi.ExecutionControl;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class D3 {
+    public static void main(String[] args) {
+        InputStream is = d1.class.getClassLoader().getResourceAsStream("d3b.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        var lines = reader.lines().toList();
+
+        StringBuilder all = new StringBuilder();
+        for (String l : lines) { all.append(l); }
+
+        Pattern pattern = Pattern.compile("mul\\([0-9]+,[0-9]+\\)");
+        long acc = 0;
+        var count = 0;
+        Matcher matcher = pattern.matcher(all.toString());
+        while (matcher.find()) {
+            String mul = matcher.group();
+            System.out.println(mul);
+            count++;
+
+            var ints = Arrays.stream(mul.substring(4, mul.length() - 1).split(",")).map(Integer::valueOf).toList();
+            acc = acc + (ints.get(0) * ints.get(1));
+        }
+        System.out.println(count);
+        System.out.println(acc);
+    }
+}
